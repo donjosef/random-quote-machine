0b043d707c017fad3f68e1ebed4a7ab35bae5a74
donjoe <giumontanaro@gmail.com>
2018-12-21 19:21:55 +0100

    Use instance of Quote component inside App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+
+import Quote from './components/Quote/Quote';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div className="App" id="quote-box">
+        <Quote />
       </div>
     );
   }
