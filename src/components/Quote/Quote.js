f4ee0d458952600748a0b2267ef97d5e08e0b270
donjoe <giumontanaro@gmail.com>
2018-12-21 21:57:10 +0100

    Give a class QUote to div and margin bottom

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Quote(props) {
     const { quote } = props; //quote is an object received by App as prop
   return (
-    <div>
+    <div className="Quote">
       <p id="text" className="text">{quote ? quote.quote : null}</p>
       <span className="author" id="author">&minus; {quote ? quote.author : null}</span>
     </div>
