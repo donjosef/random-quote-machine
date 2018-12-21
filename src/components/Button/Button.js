aff8088fa80190ae7aa189a4712727d03c8b771b
donjoe <giumontanaro@gmail.com>
2018-12-21 21:29:57 +0100

    Create Button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRetweet } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faRetweet)
+
+export default function Button(props) {
+    const { type } = props;
+    let btn;
+    let btnClass;
+    if (type === 'tweet') {
+        btnClass = 'btn-tweet';
+        btn = <a
+            className={btnClass}
+            id="tweet-quote" href="#"
+            title="Tweet this quote!">
+            <FontAwesomeIcon icon="retweet" />
+        </a>
+    }
+
+    if (type === 'new-quote') {
+        btnClass = 'btn-new-quote';
+        btn = <button className={btnClass} id="new-quote">New Quote</button>
+    }
+
+    return btn;
+}
