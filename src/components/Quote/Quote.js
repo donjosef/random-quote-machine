69475c95b899b17de971ac8c56dce14a77660201
donjoe <giumontanaro@gmail.com>
2018-12-21 21:49:26 +0100

    Change style of text and author

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -4,7 +4,7 @@ export default function Quote(props) {
     const { quote } = props; //quote is an object received by App as prop
   return (
     <div>
-      <p id="text">{quote ? quote.quote : null}</p>
+      <p id="text" className="text">{quote ? quote.quote : null}</p>
       <span className="author" id="author">&minus; {quote ? quote.author : null}</span>
     </div>
   )
