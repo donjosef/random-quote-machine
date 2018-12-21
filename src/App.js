6abaa05a71ce730e704fd6a6deaec85e7d1e21c6
donjoe <giumontanaro@gmail.com>
2018-12-21 21:42:28 +0100

    When component mounts fetch from restful APi and display the response in Quote component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import Button from './components/Button/Button';
 import './App.css';
 
 class App extends Component {
+  state = {
+    quote: null
+  }
+
+  componentDidMount() {
+    fetch('https://talaikis.com/api/quotes/random/')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({quote: data})
+    })
+  }
+
   render() {
     return (
       <div className="App" id="quote-box">
-        <Quote />
+        <Quote quote={this.state.quote}/>
         <Button type="tweet"/>
         <Button type="new-quote"/>
       </div>
--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export default function Quote(props) {
-    const { quote, author } = props
+    const { quote } = props; //quote is an object received by App as prop
   return (
     <div>
-      <p id="text">{quote}</p>
-      <span className="author" id="author">&minus; {author}</span>
+      <p id="text">{quote ? quote.quote : null}</p>
+      <span className="author" id="author">&minus; {quote ? quote.author : null}</span>
     </div>
   )
 }
