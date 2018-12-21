06bac3b7512d786d19174f33f639d61ab4df1dfb
donjoe <giumontanaro@gmail.com>
2018-12-21 22:02:30 +0100

    Add quote-left icon to text

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faQuoteLeft)
+
 export default function Quote(props) {
     const { quote } = props; //quote is an object received by App as prop
   return (
     <div className="Quote">
-      <p id="text" className="text">{quote ? quote.quote : null}</p>
+      <p id="text" className="text">
+        <FontAwesomeIcon icon="quote-left" />
+        {quote ? ' ' + quote.quote : null}
+      </p>
       <span className="author" id="author">&minus; {quote ? quote.author : null}</span>
     </div>
   )
