d6419547a0c90dd2f5c8c7eb41832de74495274e
donjoe <giumontanaro@gmail.com>
2018-12-22 13:03:53 +0100

    Set state of randomColor on mounts, pass it as bg to wrapper

--- src/App.js
+++ src/App.js
@@ -6,19 +6,34 @@ import './App.css';
 
 class App extends Component {
   state = {
-    quote: null
+    quote: null,
+    randomColor: null
   }
 
   componentDidMount() {
     this.getQuote();
+
+    const r = this.toHexColor(Math.floor(Math.random() * 256));
+    const g = this.toHexColor(Math.floor(Math.random() * 256));
+    const b = this.toHexColor(Math.floor(Math.random() * 256));
+    this.setState({ randomColor: `#${r}${g}${b}` });
+
+  }
+
+  toHexColor = (n) => {
+    /* Avoid number with one digit */
+    if (n < 16) {
+      return '0' + Math.abs(n).toString(16);
+    }
+    return Math.abs(n).toString(16);
   }
 
   getQuote = () => {
     fetch('https://talaikis.com/api/quotes/random/')
-    .then(res => res.json())
-    .then(data => {
-      this.setState({quote: data})
-    });
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ quote: data })
+      });
   }
 
   getQuoteHandler = () => {
@@ -27,10 +42,12 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App" id="quote-box">
-        <Quote quote={this.state.quote}/>
-        <Button type="tweet" text={this.state.quote ? this.state.quote.quote : ''}/>
-        <Button type="new-quote" click={this.getQuoteHandler}/>
+      <div className="wrapper" style={{background: this.state.randomColor}}>
+        <div className="App" id="quote-box">
+          <Quote quote={this.state.quote} />
+          <Button type="tweet" text={this.state.quote ? this.state.quote.quote : ''} />
+          <Button type="new-quote" click={this.getQuoteHandler} />
+        </div>
       </div>
     );
   }
