d0bac1e06ad29aa0da0806040e8b0d5985b16d41
donjoe <giumontanaro@gmail.com>
2018-12-22 14:54:45 +0100

    Updates

---
+++ precache-manifest.72c2cdcc98492dd69ae801aaf5b88679.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8507572375d5b5ea7f9",
+    "url": "/random-quote-machine/static/js/runtime~main.b8507572.js"
+  },
+  {
+    "revision": "e9602bfa6f5b03280b9b",
+    "url": "/random-quote-machine/static/js/main.e9602bfa.chunk.js"
+  },
+  {
+    "revision": "baa21923ace44e345d82",
+    "url": "/random-quote-machine/static/js/1.baa21923.chunk.js"
+  },
+  {
+    "revision": "e9602bfa6f5b03280b9b",
+    "url": "/random-quote-machine/static/css/main.3d8e3d20.chunk.css"
+  },
+  {
+    "revision": "4549ae9c1792e1de9c953d9960289ce2",
+    "url": "/random-quote-machine/index.html"
+  }
+];
